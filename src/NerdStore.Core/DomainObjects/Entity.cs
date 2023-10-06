5a63dd7968468dcd102239a22ec570b1f4837f3b
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2023-10-06 20:24:36 -0300

    Criando o microsserviço de catalogo

---
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(null, b))
+                return true;
+
+            if (ReferenceEquals(null, a) || ReferenceEquals(null, b))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
