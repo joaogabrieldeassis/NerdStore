3a1bc35b23ce55daa65560aa56ae72e02ab57f09
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2024-03-18 20:14:51 -0300

    Criando o catalogo da loja

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-<b>Desenho da solução</b>
+<b>Desenho da modelagem</b>
 <br></br>
-![Desenho Da Solução](https://github.com/joaogabrieldeassis/NerdStore/assets/89229808/c2d51692-50a9-4b67-8fa3-1afb5e55b86f)
+![](https://github.com/joaogabrieldeassis/NerdStore/assets/89229808/c2d51692-50a9-4b67-8fa3-1afb5e55b86f)
--- src/NerdStore.Core/DomainObjects/Entity.cs
+++ src/NerdStore.Core/DomainObjects/Entity.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace NerdStore.Core.DomainObjects
 {
-    public abstract class Entity
+    public class Entity
     {
         public Entity()
         {
             Id = Guid.NewGuid();
         }
+
         public Guid Id { get; set; }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             var compareTo = obj as Entity;
+
             if (ReferenceEquals(this, compareTo)) return true;
             if (ReferenceEquals(null, compareTo)) return false;
 
@@ -25,10 +22,10 @@ namespace NerdStore.Core.DomainObjects
 
         public static bool operator ==(Entity a, Entity b)
         {
-            if (ReferenceEquals(a, null) && ReferenceEquals(null, b))
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
                 return true;
 
-            if (ReferenceEquals(null, a) || ReferenceEquals(null, b))
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
                 return false;
 
             return a.Equals(b);
@@ -37,6 +34,16 @@ namespace NerdStore.Core.DomainObjects
         public static bool operator !=(Entity a, Entity b)
         {
             return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
         }
     }
 }
